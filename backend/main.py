e531daa9a740ece81dd6c18c2aa0a22170fcbc1d
inversionestt <34720095+inversionestt@users.noreply.github.com>
2026-03-16 21:01:25 -0400

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -2,16 +2,32 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+watchlist = [
+    "AAPL",
+    "MSFT",
+    "GOOGL",
+    "AMZN",
+    "META",
+    "TSLA",
+    "NVDA"
+]
+
 @app.get("/")
 def home():
     return {"status": "Options Bot Running"}
 
 @app.get("/scan")
 def scan():
-    return {
-        "symbol": "AAPL",
-        "strategy": "CSP",
-        "strike": 210,
-        "premium": 2.35,
-        "roi": "2.4%"
-    }
+
+    results = []
+
+    for symbol in watchlist:
+        results.append({
+            "symbol": symbol,
+            "strategy": "CSP",
+            "strike": 210,
+            "premium": 2.35,
+            "roi": "2.4%"
+        })
+
+    return results
